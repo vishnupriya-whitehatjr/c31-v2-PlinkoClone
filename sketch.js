4132b41a1b950d88f6deaffac23a8075a171cef8
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-05-11 17:34:34 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -11,8 +11,8 @@ function setup() {
   createCanvas(600, 700);
   engine = Engine.create();
   world = engine.world;
-
-  for (var j = 50; j < 700; j=j+100) {
+  ground = new Ground(300,690,600,20);
+  for (var j = 50; j < 600; j=j+100) {
     for (var i = 50; i < 600 + 1; i=i+100) {
      plinkos.push(new Plinko(i,j));
     }
@@ -22,6 +22,7 @@ function setup() {
 function draw() {
   background("black");
   Engine.update(engine);
+  ground.display();
    for (var i = 0; i < plinkos.length; i++) {
      plinkos[i].display();
    }
