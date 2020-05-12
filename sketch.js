94de0af9d05ba7694c4567cc4d662a1769fdfbab
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-05-12 14:32:49 +0530

    score
    adding score as the particle generates

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var particles = [];
 var plinkos = [];
 var divisions =[];
 var divisionHeight=300;
-
+var score =0;
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
@@ -53,19 +53,27 @@ function setup() {
 
 function draw() {
   background("black");
+  textSize(20)
+  text("Score : "+score,20,30);
   Engine.update(engine);
   ground.display();
+  
    for (var i = 0; i < plinkos.length; i++) {
+     
      plinkos[i].display();
+     
    }
    if(frameCount%60===0){
      particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
    }
-  
-   for (var j = 0; j < particles.length; j++) {
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
      particles[j].display();
    }
    for (var k = 0; k < divisions.length; k++) {
+     
      divisions[k].display();
    }
 }
